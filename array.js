dd0cd69ebda912576cefa164518e320ba7e42599
unknown <py8116937@gmail.com>
2024-11-20 18:02:02 +0530

    update

---
+++ array.js
@@ -0,0 +1,57 @@
+//let a=[1,2,3,4,5,6,7,8,9]
+
+//console.log(a[0])
+//let [b,c,v,x,y,z,m]=a
+//console.log(b)
+//console.log(c)
+//console.log(v)
+//console.log(y)
+//console.log(x)
+
+//let b=a.push(12,15,10,5)
+//console.log(a)
+
+
+//let b=a.pop(10,5)
+ //console.log(a)
+
+//let b=a.unshift(9,78)
+ //console.log(a)
+
+//let b=a.shift()
+//console.log(a)
+
+let a=[1,2,3,4,5,7,8]
+let b= a.slice(0,4)
+//let p= a.slice(4,8)
+console.log(b)
+//let a=[1,2,3,4,5,6,7,8]
+//let b=a.splice(5,2)
+//console.log(a)
+//let c=a.splice(6,2)
+//console.log(a)
+
+//for replacing
+
+//let b=a.splice(5,2,"op")
+//console.log(a)
+//for adding
+// let a =[1,2,3,4,5,6,7,8]
+// let b=a.splice (4,0,"op")
+// let l=a.splice(7,0,"op")
+// let m=a.splice (2,0,"hey")
+// console.log(a)
+
+//let789n for bigint
+
+// let a =["Krishna","Radha","Ram","Siya"]
+// let b=a.splice(3,0,"Sita")
+// console.log(a)
+
+
+
+
+
+
+
+
