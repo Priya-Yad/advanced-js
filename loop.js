02ebfe442a7e372a7bcb71594716e2608e391fcf
unknown <py8116937@gmail.com>
2024-11-20 18:13:50 +0530

    firstupdate

---
+++ loop.js
@@ -0,0 +1,46 @@
+let a=[1,2,3,4,5,6,7,8,9,10]
+
+// //let t=[]
+// for(let i=0;i<a.length;i++){
+//     //console.log(a)
+    
+// }
+// console.log(a)
+
+// for multiplying the array with 2(to print value we use a[i], for printing index use [i])
+
+// let t=[]
+// for(let i=0;i<a.length;i++){
+//     t.push(a[i]*2)
+
+// }
+// console.log(t)
+
+//for i loop(use for index only)
+
+// for(let i in a){
+//     console.log(a[i])
+// }
+
+//for of loop(only value return)
+// for (let i of a){
+//     console.log(i)
+// }
+//for reverse
+// for(let i=a.length;i>=0;i--){
+//     console.log(a[i])
+// }
+
+//for undefined
+
+// for(let i=a.length-1;i>=0;i--){
+//     console.log(a[i])
+// }
+
+// for each loop(do not return any array)it use item 
+a.forEach((item)=>{ //=>fat arrow for writting condition in single line
+    console.log(item)
+})
+
+
+
